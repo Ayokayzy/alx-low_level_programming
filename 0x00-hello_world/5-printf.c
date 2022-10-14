e42b7f6b7066344fd58674c55842ddc886af15da
Ayokayzy <theayokayzy1@gmail.com>
2022-10-14 04:24:22 +0000

    printf done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-/*
-* Entry point
+/**
+* main - Entry point
 *
-*Return: Always 0 (Success)
+* Return: Always 0 (Success)
 */
 
 int main(void)
 {
 	/* prints to stdout using printf */
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
