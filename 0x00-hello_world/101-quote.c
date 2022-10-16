48fcb8e0680cd002ce67732531d4f1ecc42223b7
Ayokayzy <theayokayzy1@gmail.com>
2022-10-16 21:15:39 +0000

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: print
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10019");
+	return (1);
+}
