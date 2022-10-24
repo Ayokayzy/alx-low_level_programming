4e9edcb2a0e661c705b5da4a90610ccacde7e506
Ayokayzy <theayokayzy1@gmail.com>
2022-10-24 14:26:12 +0000

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strlen - Entry point
+* @s: pointer string
+* Description: return the lenth of string
+* Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		return (i);
+	}
