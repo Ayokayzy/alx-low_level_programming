4bf486f82c319c2ea41376c75acdc2e4a3289af1
Ayokayzy <theayokayzy1@gmail.com>
2022-10-24 14:01:11 +0000

    swap int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* swap_int - Entry point
+* @a: pointer variable
+* @b: pointer variable
+* Description: Swap value of two integers
+* Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
