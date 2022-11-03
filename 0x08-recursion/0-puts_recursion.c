8452d6abafd196db46201be16ceec6167f5a116e
Ayokayzy <theayokayzy1@gmail.com>
2022-11-03 22:59:10 +0000

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,14 +3,14 @@
 /**
 * _puts_recursion - this function prints a string, followed by a new line
 * @s: the string to be printed
-* Return: 
+* Return: void
 */
 
 void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar(s);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else
