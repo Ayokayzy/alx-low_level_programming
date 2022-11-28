14472b6b56367e5c277a5c1f60c33e5df2d6262a
Ayokayzy <theayokayzy1@gmail.com>
2022-11-28 16:54:44 +0000

    string concat correctio

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,6 +16,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int s1len, s2len, idx;
 	char *newstr;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	s1len = 0;
 	while (s1[s1len])
 		s1len++;
