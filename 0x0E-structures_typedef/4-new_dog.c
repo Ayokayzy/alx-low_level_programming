09986364b5cf71f7275e26d8fe52484b32e2e1d6
Ayokayzy <theayokayzy1@gmail.com>
2022-12-01 03:11:19 +0000

    print dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * *new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ * Return: Null if function fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	
