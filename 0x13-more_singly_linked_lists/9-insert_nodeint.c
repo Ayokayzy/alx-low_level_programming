888c3e30eefbff7f84cde5a8f181499438c4aa68
Ayokayzy <theayokayzy1@gmail.com>
2023-07-07 02:50:19 +0000

    add node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 		i++;
 	}
-	if (i > idx)
+	if (i < idx)
 		return (NULL);
 
 	temp = current->next;
