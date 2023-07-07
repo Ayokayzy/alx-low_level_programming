76718b63e73f5950bd809504f070ce25b5c93843
Ayokayzy <theayokayzy1@gmail.com>
2023-07-07 02:36:29 +0000

    pop list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,7 +12,7 @@ int pop_listint(listint_t **head)
 	listint_t *first_node;
 	int temp;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (0);
 
 	first_node = *head;
@@ -24,4 +24,3 @@ int pop_listint(listint_t **head)
 
 	return (temp);
 }
-	
