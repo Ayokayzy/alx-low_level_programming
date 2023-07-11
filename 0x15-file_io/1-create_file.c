03b278e52a55bfe7e0837ae53fab49f278a4ecf8
Ayokayzy <theayokayzy1@gmail.com>
2023-07-11 11:34:11 +0000

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,8 +3,8 @@
 /**
  * create_file - a function that creates a file.
  * @filename: the name of the file to create
- * @text_context: a NULL terminated string to write to the file
- * Returns: 1 on success, -1 on failure
+ * @text_content: a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
