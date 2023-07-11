3e7a3ef3a30c2b44302d9a7b8e241b87c0a5514b
Ayokayzy <theayokayzy1@gmail.com>
2023-07-11 11:31:57 +0000

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,12 +13,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while(*text_content++)
+		while (text_content[strlen])
 			strlen++;
 	}
 	/* printf("strlen %ld", strlen); */
 	if (filename == NULL)
-		return(-1);
+		return (-1);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	fw = write(fd, text_content, strlen);
