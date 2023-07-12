e0980ff1b92f9edd0d80d642ad3570a0af31c882
Ayokayzy <theayokayzy1@gmail.com>
2023-07-12 01:35:42 +0000

    copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	int fd_to, fd_from, fw, fr, c_from, c_to;
 	char *buff;
 
-	if (argc < 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
@@ -22,30 +22,54 @@ int main(int argc, char *argv[])
 	fd_from = open(argv[1], O_RDONLY);
 	fd_to = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
 
-	buff = malloc(sizeof(char) * 1024);
-	fr = read(fd_from, buff, 1024);
-
-	if (fd_from == -1 || fr == -1)
+	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[0]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	fw = write(fd_to, buff, fr);
-	if (fd_to == -1 || fw == -1)
+	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		exit(98);
+	}
+
+	buff = malloc(sizeof(char) * 1024);
+
+	while (fr == 1024)
+	{
+		fr = read(fd_from, buff, 1024);
+
+		if (fd_from == -1 || fr == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			free(buff);
+			exit(98);
+		}
+
+		fw = write(fd_to, buff, fr);
+		if (fd_to == -1 || fw == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buff);
+			exit(99);
+		}
 	}
 
 	free(buff);
 	c_from = close(fd_from);
 	c_to = close(fd_to);
 
-	if (c_from == -1 || c_to == -1)
+	if (c_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (c_from || c_to));
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c_from);
 		exit(100);
+	}
+
+	if (c_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c_to);
+		exit(98);
 	}
 
 	return (0);
